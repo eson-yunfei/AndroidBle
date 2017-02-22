99478d86b38a6af5b460a61be259924cf51a707c
Arboxu <xujianbo1992@gmail.com>
2017-02-22 18:39:21 +0800

    增加主页面，设置页面

--- app/src/main/java/org/eson/liteble/MainActivity.java
+++ app/src/main/java/org/eson/liteble/MainActivity.java
@@ -1,13 +1,98 @@
 package org.eson.liteble;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+import org.eson.liteble.activity.SettingActivity;
+import org.eson.liteble.adapter.ScanBLEAdapter;
+import org.eson.liteble.bean.BleItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+	private Button searchBtn;
+	private ListView mListView;
+
+	private List<BleItem> deviceList = new ArrayList<>();
+	private ScanBLEAdapter scanBLEAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		searchBtn = (Button) findViewById(R.id.start_search);
+		mListView = (ListView) findViewById(R.id.listview);
+		initView();
 	}
+
+	private void initView(){
+
+		for (int i = 0; i < 5; i++) {
+			BleItem bleItem = new BleItem();
+			bleItem.setAddress("test mac");
+			bleItem.setName("name");
+			bleItem.setRssi((int) (Math.random()*100));
+			deviceList.add(bleItem);
+		}
+		//设置列表
+		scanBLEAdapter = new ScanBLEAdapter(this, deviceList);
+		mListView.setAdapter(scanBLEAdapter);
+
+		mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+//				if (loadingDialog.isShowing()) {
+//					return;
+//				}
+//				loadingDialog.show();
+
+				BleItem device = deviceList.get(i);
+				//TODO  连接蓝牙设备
+
+			}
+		});
+	}
+
+	private void startToSetting(){
+		Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+		startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			LogUtil.e("setting ~~~~ ");
+			startToSetting();
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
 }
---
+++ app/src/main/java/org/eson/liteble/adapter/ScanBLEAdapter.java
@@ -0,0 +1,67 @@
+package org.eson.liteble.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.eson.liteble.LogUtil;
+import org.eson.liteble.R;
+import org.eson.liteble.bean.BleItem;
+
+import java.util.List;
+
+/**
+ * @name AndroidBle
+ * @class name：org.eson.liteble.adapter
+ * @class describe
+ * @anthor xujianbo E-mail: xuarbo@qq.com
+ * @time 2017/2/22 15:40
+ * @change
+ * @chang time
+ * @class describe
+ */
+public class ScanBLEAdapter extends MyBaseAdapter<BleItem> {
+
+    public ScanBLEAdapter(Context context, List<BleItem> dataList) {
+        super(context, dataList);
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder;
+        if (view == null) {
+            view = inflater.inflate(R.layout.item_scan_device, null);
+            viewHolder = new ViewHolder(view);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        BleItem device = dataList.get(position);
+        String name = device.getName();
+        LogUtil.d("信号:"+device.getRssi());
+        LogUtil.e("name = " + name);
+        String mac = device.getAddress();
+        viewHolder.deviceRssi.setText("信号强度："+device.getRssi());
+        viewHolder.deviceName.setText(name);
+        viewHolder.deviceMac.setText(mac);
+
+        return view;
+    }
+
+    class ViewHolder{
+        View rootView;
+
+        TextView deviceName;
+        TextView deviceMac;
+        TextView deviceRssi;
+
+        ViewHolder(View rootView) {
+
+            this.rootView = rootView;
+            this.deviceName = findView(rootView, R.id.deviceName);
+            this.deviceMac = findView(rootView, R.id.deviceMac);
+            this.deviceRssi = findView(rootView, R.id.deviceRssi);
+        }
+    }
+}
