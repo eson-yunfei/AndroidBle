328719f93645c40820f6f0f1617215b4ee0de9a0
xiaoyunfei <1668904979@qq.com>
2017-02-23 12:01:17 +0800

    添加 蓝牙连接SDK

--- blesdk/src/main/java/org/eson/ble_sdk/control/BLEControl.java
+++ blesdk/src/main/java/org/eson/ble_sdk/control/BLEControl.java
@@ -1,16 +1,7 @@
 package org.eson.ble_sdk.control;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
-import android.bluetooth.BluetoothGattService;
 import android.content.Context;
-import android.text.TextUtils;
-
-import org.eson.ble_sdk.BLESdk;
 
 import java.util.UUID;
 
@@ -20,7 +11,7 @@ import java.util.UUID;
  * @说明：
  */
 
-public class BLEControl {
+public class BLEControl extends BLEBaseControl {
 
 	private BLEControl() {
 
@@ -40,8 +31,6 @@ public class BLEControl {
 		return bleControl;
 	}
 
-	private BluetoothAdapter bluetoothAdapter = null;
-	private BluetoothGatt bluetoothGatt;
 
 	/**
 	 * 连接设备
@@ -50,64 +39,20 @@ public class BLEControl {
 	 * @param deviceMac
 	 * @param isAutoConnect
 	 */
-	public void connectToDevice(Context context, String deviceMac, boolean isAutoConnect) {
+	public void connectToDevice(Context context, String deviceMac, boolean isAutoConnect,
+								BLEConnectCallBack connectCallBack) {
 
-		if (TextUtils.isEmpty(deviceMac)) {
-			return;
-		}
-
-		if (bluetoothGatt != null) {
-			bluetoothGatt.connect();
-			return;
-		}
-		if (bluetoothAdapter == null) {
-			bluetoothAdapter = BLESdk.get().getBluetoothAdapter();
-		}
-
-		BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceMac);
-		if (device == null) {
-			return;
-		}
-
-		bluetoothGatt = device.connectGatt(context, isAutoConnect, gattCallback);
-		bluetoothGatt.connect();
+		BLEConnection.get().connectionDevice(context, deviceMac, isAutoConnect, connectCallBack);
 	}
 
-	BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
-		@Override
-		public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-			super.onConnectionStateChange(gatt, status, newState);
-		}
 
-		@Override
-		public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-			super.onServicesDiscovered(gatt, status);
-		}
+	/**
+	 * 断开蓝牙连接
+	 */
+	public void disConnect() {
 
-		@Override
-		public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-			super.onCharacteristicRead(gatt, characteristic, status);
-		}
-
-		@Override
-		public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-			super.onCharacteristicWrite(gatt, characteristic, status);
-
-			if (status == BluetoothGatt.GATT_SUCCESS){
-				//发送成功
-			}
-		}
-
-		@Override
-		public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
-			super.onCharacteristicChanged(gatt, characteristic);
-
-			byte[] notice = characteristic.getValue();
-
-
-		}
-	};
-
+		BLEConnection.get().disConnection();
+	}
 
 	/**
 	 * 发送数据
@@ -116,21 +61,11 @@ public class BLEControl {
 	 * @param characteristicUuid
 	 * @param data
 	 */
-	public void sendData(UUID serviceUuid, UUID characteristicUuid, byte[] data) {
+	public void sendData(UUID serviceUuid, UUID characteristicUuid,
+						 byte[] data, BLEDataTransCallBack bleDataTransCallBack) {
 
-		if (bluetoothGatt == null) {
-			return;
-		}
-		BluetoothGattService service = bluetoothGatt.getService(serviceUuid);
-		if (service == null) {
-			return;
-		}
-		BluetoothGattCharacteristic characteristic = service.getCharacteristic(characteristicUuid);
-		if (characteristic == null) {
-			return;
-		}
-		characteristic.setValue(data);
-		bluetoothGatt.writeCharacteristic(characteristic);
+		BLEDataTransport.get().sendData(serviceUuid, characteristicUuid, data, bleDataTransCallBack);
+
 	}
 
 
@@ -141,27 +76,15 @@ public class BLEControl {
 	 * @param characteristicUuid
 	 * @param descriptorUuid
 	 */
-	public void enableNotify(UUID serviceUuid, UUID characteristicUuid, UUID descriptorUuid) {
+	public void enableNotify(UUID serviceUuid, UUID characteristicUuid,
+							 UUID descriptorUuid, BLEDataTransCallBack bleDataTransCallBack) {
 
-		if (bluetoothGatt == null) {
-			return;
-		}
+		BLEDataTransport.get().enableNotify(serviceUuid, characteristicUuid, descriptorUuid, bleDataTransCallBack);
 
-		BluetoothGattService service = bluetoothGatt.getService(serviceUuid);
-		if (service == null) {
-			return;
-		}
 
-		BluetoothGattCharacteristic characteristic = service.getCharacteristic(characteristicUuid);
-		if (characteristic == null) {
-			return;
-		}
+	}
 
-		bluetoothGatt.setCharacteristicNotification(characteristic, true);//激活通知
-
-		BluetoothGattDescriptor descriptor = characteristic.getDescriptor(descriptorUuid);
-		descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
-		bluetoothGatt.writeDescriptor(descriptor);
-
+	public BluetoothGatt getBluetoothGatt() {
+		return bluetoothGatt;
 	}
 }
--- blesdk/src/main/java/org/eson/ble_sdk/control/BLEDataTransCallBack.java
+++ blesdk/src/main/java/org/eson/ble_sdk/control/BLEDataTransCallBack.java
@@ -1,29 +1,16 @@
 package org.eson.ble_sdk.control;
 
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-
 /**
  * @作者 xiaoyunfei
  * @日期: 2017/2/22
  * @说明：
  */
 
-public class BLEDataTransCallBack extends BluetoothGattCallback {
+public interface BLEDataTransCallBack {
 
-	@Override
-	public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-		super.onCharacteristicRead(gatt, characteristic, status);
-	}
+	void onCharacteristicRead();
 
-	@Override
-	public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-		super.onCharacteristicWrite(gatt, characteristic, status);
-	}
+	void onCharacteristicWrite();
 
-	@Override
-	public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
-		super.onCharacteristicChanged(gatt, characteristic);
-	}
+	void onNotify(byte[] data);
 }
