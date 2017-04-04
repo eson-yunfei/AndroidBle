dc977b4ba98ec9fd9a3088af384e3016d0996cb1
xiaoyunfei <1668904979@qq.com>
2017-04-04 18:30:19 +0800

    添加 Copyright

--- app/src/main/java/org/eson/liteble/activity/BaseActivity.java
+++ app/src/main/java/org/eson/liteble/activity/BaseActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2017. xiaoyunfei
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
 package org.eson.liteble.activity;
 
 import android.content.Context;
@@ -14,40 +30,40 @@ import android.view.View;
 
 abstract class BaseActivity extends AppCompatActivity implements View.OnClickListener {
 
-	protected Context mContext = null;
+    protected Context mContext = null;
 
-	@Override
-	protected void onCreate(@Nullable Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(getRootLayout());
-		mContext = this;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getRootLayout());
+        mContext = this;
 
-		initView();
-		initViewListener();
+        initView();
+        initViewListener();
 
-		process(savedInstanceState);
-	}
+        process(savedInstanceState);
+    }
 
-	protected void initView() {
+    protected void initView() {
 
-	}
+    }
 
-	protected void initViewListener() {
-	}
+    protected void initViewListener() {
+    }
 
-	protected void process(Bundle savedInstanceState) {
+    protected void process(Bundle savedInstanceState) {
 
-	}
+    }
 
-	protected abstract int getRootLayout();
+    protected abstract int getRootLayout();
 
-	@Override
-	public void onClick(View v) {
+    @Override
+    public void onClick(View v) {
 
-	}
+    }
 
 
-	protected <T extends View> T findView(int viewId) {
-		return (T) findViewById(viewId);
-	}
+    protected <T extends View> T findView(int viewId) {
+        return (T) findViewById(viewId);
+    }
 }
