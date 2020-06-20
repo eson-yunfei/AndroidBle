2fbc2ae6cad6c918bf7193c15262f9c0f45fa897
xiaoyunfei <xyf861448949@163.com>
2020-06-20 18:08:50 +0800

    修改 绑定列表，服务列表

---
+++ app/src/main/java/org/eson/liteble/activity/vms/ServiceListViewModel.java
@@ -0,0 +1,135 @@
+package org.eson.liteble.activity.vms;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+
+import com.e.ble.util.BLE_UUID_Util;
+
+import org.eson.liteble.bean.CharacterBean;
+import org.eson.liteble.bean.DescriptorBean;
+import org.eson.liteble.bean.ServiceBean;
+import org.eson.liteble.util.LogUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static android.bluetooth.BluetoothGattCharacteristic.PROPERTY_NOTIFY;
+import static android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ;
+import static android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE;
+
+/**
+ * Auth : xiao.yunfei
+ * Date : 2020/6/20 17:51
+ * Package name : org.eson.liteble.activity.vms
+ * Des :
+ */
+public class ServiceListViewModel extends ViewModel {
+
+    public List<ServiceBean> getServiceList(BluetoothGatt bluetoothGatt) {
+
+
+        List<BluetoothGattService> serviceArrayList = bluetoothGatt.getServices();
+
+        if (serviceArrayList == null || serviceArrayList.size() == 0) {
+            return null;
+        }
+
+
+        List<ServiceBean> serviceBeanList = new ArrayList<>();
+
+        ServiceBean serviceBean;
+        for (BluetoothGattService service : serviceArrayList) {
+            serviceBean = new ServiceBean();
+            UUID serviceUUID = service.getUuid();
+            String typeStr = getServerType(serviceBean, service, serviceUUID);
+
+            serviceBean.setServiceUUID(serviceUUID.toString());
+            serviceBean.setServiceType(typeStr);
+
+            List<BluetoothGattCharacteristic> gattCharacteristics = service.getCharacteristics();
+
+            if (gattCharacteristics == null || gattCharacteristics.size() == 0) {
+                serviceBeanList.add(serviceBean);
+                continue;
+            }
+
+            List<CharacterBean> characterBeanList = getCharacters(serviceUUID,gattCharacteristics);
+            serviceBean.setUUIDBeen(characterBeanList);
+            serviceBeanList.add(serviceBean);
+        }
+
+        return serviceBeanList;
+    }
+
+    private String getServerType(ServiceBean serviceBean, BluetoothGattService service, UUID serviceUUID) {
+
+        int serviceType = service.getType();
+        return (serviceType == BluetoothGattService.SERVICE_TYPE_PRIMARY)
+                ? "PRIMARY" : "SECONDARY";
+    }
+
+    private List<CharacterBean> getCharacters(@NonNull UUID serviceUUID,
+                                              @NonNull List<BluetoothGattCharacteristic> gattCharacteristics){
+
+        List<CharacterBean> characterBeanList = new ArrayList<>();
+
+        CharacterBean characterBean;
+        for (BluetoothGattCharacteristic gattCharacteristic : gattCharacteristics) {
+            characterBean = new CharacterBean();
+
+            UUID character = gattCharacteristic.getUuid();
+            String characterString = character.toString();
+            characterBean.setCharacterUUID(characterString);
+            characterBean.setServiceUUID(serviceUUID.toString());
+
+            int characterValue = BLE_UUID_Util.getValue(character);
+
+            LogUtil.e("character:" + character);
+            LogUtil.e("characterValue:" + Integer.toHexString(characterValue));
+
+            int properties = gattCharacteristic.getProperties();    //用于区分特性用途（读、写、通知）
+
+            if ((properties & PROPERTY_READ) != 0) {
+                characterBean.setRead(true);
+            }
+            if ((properties & PROPERTY_WRITE) != 0) {
+                characterBean.setWrite(true);
+            }
+            if ((properties & PROPERTY_NOTIFY) != 0) {
+                characterBean.setNotify(true);
+            }
+
+            List<BluetoothGattDescriptor> descriptorList = gattCharacteristic.getDescriptors();
+
+            if (descriptorList == null || descriptorList.size() == 0) {
+                characterBeanList.add(characterBean);
+                continue;
+            }
+            List<DescriptorBean> descriptorBeen = getDescriptors(descriptorList);
+            characterBean.setDescriptorBeen(descriptorBeen);
+            characterBeanList.add(characterBean);
+        }
+        return characterBeanList;
+    }
+
+    private List<DescriptorBean> getDescriptors(@NonNull List<BluetoothGattDescriptor> gattDescriptors){
+        List<DescriptorBean> descriptorBeen = new ArrayList<>();
+        DescriptorBean descriptorBean;
+        for (BluetoothGattDescriptor gattDescriptor : gattDescriptors) {
+            UUID des = gattDescriptor.getUuid();
+            int permissions = gattDescriptor.getPermissions();
+            descriptorBean = new DescriptorBean();
+            descriptorBean.setUUID(des.toString());
+            descriptorBean.setPermissions(permissions);
+            descriptorBeen.add(descriptorBean);
+        }
+        return descriptorBeen;
+    }
+
+}
